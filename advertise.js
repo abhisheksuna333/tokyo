fe0d870b4ba4e735d4b3996832f53e17f4a25703
abhisheksuna333 <65883904+abhisheksuna333@users.noreply.github.com>
2021-07-17 01:28:05 +0530

    Add files via upload

---
+++ advertise.js
@@ -0,0 +1,10 @@
+document.write( '<marquee>This test is sponsored by:- Online Guru</marquee>\n' );
+document.write( '<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n' );
+document.write( '<style>\n' );
+document.write( '.marginauto {\n' );
+document.write( '    margin: 0px auto 0px;\n' );
+document.write( '    display: block;\n' );
+document.write( '}\n' );
+document.write( '</style>\n' );
+document.write( '<a href=\"https://youtu.be/WamJ8Cu6uDE\">\n' );
+document.write( '<br><img src=\"https://s6.gifyu.com/images/VID-20210715-WA00111.gif\" class=\"marginauto\" width=\"400\"height=\"170\"></a>\n' );
