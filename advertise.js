2fa3eef2ede07d75865efeba0a5768ca5b75b594
abhisheksuna333 <65883904+abhisheksuna333@users.noreply.github.com>
2021-07-18 01:24:40 +0530

    Update advertise.js

--- advertise.js
+++ advertise.js
@@ -7,4 +7,4 @@ document.write( '    display: block;\n' );
 document.write( '}\n' );
 document.write( '</style>\n' );
 document.write( '<a href=\"https://youtu.be/WamJ8Cu6uDE\">\n' );
-document.write( '<br><img src=\"https://s6.gifyu.com/images/VID-20210715-WA00111.gif\" class=\"marginauto\" width=\"400\"height=\"170\"></a>\n' );
+document.write( '<br><img src=\"https://s6.gifyu.com/images/20210718_0121411.gif\" class=\"marginauto\" width=\"400\"height=\"170\"></a>\n' );
