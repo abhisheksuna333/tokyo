15cd8eaf9c084b3b300b926439a74b15e38d8444
abhisheksuna333 <65883904+abhisheksuna333@users.noreply.github.com>
2021-07-21 02:30:39 +0530

    Update advertise.js

--- advertise.js
+++ advertise.js
@@ -8,4 +8,4 @@ document.write( '}\n' );
 document.write( '</style>\n' );
 document.write( '<a href=\"https://ekaro.in/enkr20210720s3687004\">\n' );
 document.write( '<br><img src=\"https://1.bp.blogspot.com/-k0fFcW7pJfg/YPcUSAwiaaI/AAAAAAAAIHI/ALum0TRgRBYosHMkQC973wVREFPkrIbogCNcBGAsYHQ/s320/20210720_2040201.gif\" class=\"marginauto\" width=\"400\"height=\"170\"></a>\n' );
-S
+
