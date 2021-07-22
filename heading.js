52c5d2c5eadebf58c71f7a11b38314b468bfc2ce
abhisheksuna333 <65883904+abhisheksuna333@users.noreply.github.com>
2021-07-23 00:08:28 +0530

    Update heading.js

--- heading.js
+++ heading.js
@@ -1,4 +1,13 @@
 document.write( '<!DOCTYPE html>\n' );
+document.write( '<!-- Global site tag (gtag.js) - Google Analytics -->\n' );
+document.write( '<script async src=\"https://www.googletagmanager.com/gtag/js?id=G-VC1R9GXL7G\"></script>\n' );
+document.write( '<script>\n' );
+document.write( '  window.dataLayer = window.dataLayer || [];\n' );
+document.write( '  function gtag(){dataLayer.push(arguments);}\n' );
+document.write( '  gtag(\'js\', new Date());\n' );
+document.write( '\n' );
+document.write( '  gtag(\'config\', \'G-VC1R9GXL7G\');\n' );
+document.write( '</script>' );
 document.write( '<html>\n' );
 document.write( '\n' );
 document.write( '    \n' );
